0dbf4dd07cac7f94a341030b960a69f53eec1764
josh-benge <bengej@oregonstate.edu>
2026-04-22 09:08:12 -0400

    Completed Implementation of Intel 8080 Memory.

---
+++ src/Memory8080.cc
@@ -0,0 +1,65 @@
+// includes
+#include <array>
+#include <cstdint>
+#include <ostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+#include <filesystem>
+
+#include "Memory8080.h"
+using namespace std;
+using namespace MemoryIntel8080;
+
+Memory8080::Memory8080() : mem_buffer_{}{
+  mem_buffer_.fill(0x00);
+  end_of_ROM_ = 0x0000;
+  }
+uint8_t Memory8080::Read(uint16_t mem_location) {
+  return mem_buffer_[mem_location];
+  }
+
+void Memory8080::Write(uint16_t mem_location, uint8_t data) {
+    if (mem_location > end_of_ROM_) {
+      return;
+    }
+    mem_buffer_[mem_location] = data;
+ }
+
+void Memory8080::ClearMem() {
+  mem_buffer_.fill(0x00);
+}
+
+void Memory8080::ClearROM() {
+  fill(mem_buffer_.begin(), mem_buffer_.begin() + end_of_ROM_ + 1, 0x00);
+  end_of_ROM_ = 0x0000;
+}
+
+void Memory8080::LoadROM(string file_path) {
+  if(end_of_ROM_ != 0x0000){
+    ClearROM();
+  }
+
+    ifstream file(file_path, ios::in | ios::binary);
+
+    if(file.is_open()) {
+      uintmax_t file_size = filesystem::file_size(file_path);
+
+      if (file_size > this->SIZE) {
+        cerr << "Error: File is bigger than 64KB" << endl;
+      }
+
+      file.read(reinterpret_cast<char*>(mem_buffer_.data()), file_size);
+
+      if (file.gcount() != file_size) {
+        cerr << "Warning: File size mismatch" << endl; 
+      }
+      file.close();
+
+    } else {
+      cerr << "Error opening file." << endl;
+      return;
+    }
+
+}
---
+++ src/Memory8080.h
@@ -0,0 +1,29 @@
+#pragma once 
+#include <cstdint>
+#include <array>
+#include <string>
+
+using namespace std;
+namespace MemoryIntel8080 {
+  class Memory8080 {
+  public:
+    static constexpr uint16_t SIZE = 0xFFFF;
+    Memory8080();
+    
+    uint8_t Read(uint16_t mem_location);
+
+    void Write(uint16_t mem_loation, uint8_t data);
+
+    void ClearMem();
+
+    void ClearROM();
+    
+    void LoadROM(string file_path);
+
+  private:
+    array<uint8_t, SIZE> mem_buffer_;
+    uint16_t end_of_ROM_;
+  };
+}
+
+
