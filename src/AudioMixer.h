4e3487c8b651b606a1663fb85e7b15d1faeab77a
Brandon <arnstgob@oregonstate.edu>
2026-04-29 00:05:29 -0700

    updated styling

--- src/AudioMixer.h
+++ src/AudioMixer.h
@@ -9,47 +9,45 @@
 namespace Audio {
 
 class Mixer {
-public:
-    Mixer() {
-        Load();
-    }
+ public:
+  Mixer() { Load(); }
 
-    // Pass in Reg A (accumulator) bits for OUT port 3
-    void SetOut3(uint8_t accumulatorBits);
+  // Pass in Reg A (accumulator) bits for OUT port 3
+  void SetOut3(uint8_t accumulatorBits);
 
-    // Pass in Reg A (accumulator) bits for OUT port 5
-    void SetOut5(uint8_t accumulatorBits);
+  // Pass in Reg A (accumulator) bits for OUT port 5
+  void SetOut5(uint8_t accumulatorBits);
 
-private:
-    enum class SoundId : int {
-        Background,
-        Explosion,
-        InvaderHit,
-        Torpedo,
-        UfoHit,
-        Ufo
-    };
+ private:
+  enum class SoundId : int {
+    Background,
+    Explosion,
+    InvaderHit,
+    Torpedo,
+    UfoHit,
+    Ufo
+  };
 
-    struct Channel {
-        SDL_AudioStream* stream  = nullptr;
-        SDL_AudioSpec    spec{};
-        Uint8*           wav_buf = nullptr;
-        Uint32           wav_len = 0;
-        bool             looping = false;
-        bool             playing = false;
-    };
+  struct Channel {
+    SDL_AudioStream* stream = nullptr;
+    SDL_AudioSpec spec{};
+    Uint8* wav_buf = nullptr;
+    Uint32 wav_len = 0;
+    bool looping = false;
+    bool playing = false;
+  };
 
-    SDL_AudioDeviceID device = 0;
-    SDL_AudioSpec deviceSpec;
-    std::array<Channel, 6> channels{};
-    uint8_t prevPort3 = 0;
-    uint8_t prevPort5 = 0;
+  SDL_AudioDeviceID device = 0;
+  SDL_AudioSpec deviceSpec;
+  std::array<Channel, 6> channels{};
+  uint8_t prevPort3 = 0;
+  uint8_t prevPort5 = 0;
 
-    void Load();
+  void Load();
 
-    void Play(SoundId id);
+  void Play(SoundId id);
 
-    void Stop(SoundId id);
+  void Stop(SoundId id);
 };
 
 }  // namespace Audio
