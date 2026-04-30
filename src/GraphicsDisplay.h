c2f762121813dfc343430db0bda09e35d2a15765
Pat Keleher <122421655+PatKeleher@users.noreply.github.com>
2026-04-29 21:14:16 -0500

    Initial development for pixel display; code in main.cc used just to setup an enviroment to run/test my display class; currently trying to render a 256x224 pixel screen with all red pixels; not working correctly yet

---
+++ src/GraphicsDisplay.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SDL3/SDL.h>
+
+namespace GraphicsDisplay
+{
+    class GameWindow
+    {
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        int value;
+
+    public:
+        GameWindow(int v);
+        ~GameWindow();
+        void UpdateDisplay(const char* videoData);
+        void displayValue();
+    };
+}
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,75 @@
 #include "CPU8080.h"
 #include "Instructions8080.h"
 #include "Memory8080.h"
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <SDL3/SDL.h>
+#include "GraphicsDisplay.h"
 
-int main() { return 0; }
+using namespace std;
+
+int main()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    GraphicsDisplay::GameWindow obj(10);
+    SDL_Delay(2000);
+    obj.displayValue();
+
+    int numDrivers = SDL_GetNumRenderDrivers();
+    for (int i = 0; i < numDrivers - 1; i++)
+    {
+        cout << SDL_GetRenderDriver(i) << endl;
+    }
+
+    ifstream e_file("../invaders.e", ios::binary);
+    ifstream f_file("../invaders.f", ios::binary);
+    ifstream g_file("../invaders.g", ios::binary);
+    ifstream h_file("../invaders.h", ios::binary);
+
+    char buffer[65536] = { (char)0 };
+
+    char data;
+    uint i = 0;
+
+    while (h_file.get(data))
+    {
+        buffer[i++] = data;
+    }
+
+    while (g_file.get(data))
+    {
+        buffer[i++] = data;
+    }
+
+    while (f_file.get(data))
+    {
+        buffer[i++] = data;
+    }
+
+    while (e_file.get(data))
+    {
+        buffer[i++] = data;
+    }
+
+    int startIndex = 0x2400;
+    for (int row = 0; row < 224; row++)
+    {
+        for (int col = 0; col < 32; col++)
+        {
+            buffer[startIndex + row * 32 + col] =
+                buffer[startIndex + row * 32 + col] ^ 0xFF;
+        }
+    }
+    obj.UpdateDisplay(buffer);
+
+    SDL_Delay(15000);
+    for (int x = 0x2400; x < 0x4000; x++)
+    {
+        std::cout << std::hex << (int)(unsigned char)buffer[x] << " ";
+    }
+
+
+    SDL_Quit();
+    return 0;
+}
