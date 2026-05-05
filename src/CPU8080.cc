e72239f14934b5caa1fb078450ec213f3a438f65
josh-benge <bengej@oregonstate.edu>
2026-05-05 12:22:31 -0400

    Added a port struct and flags struct, worked on formatting

--- src/CPU8080.cc
+++ src/CPU8080.cc
@@ -4,53 +4,68 @@
 
 #include "Memory8080.h"
 
-intel_8080::CPU8080::CPU8080(std::shared_ptr<intel_8080::Memory8080> new_mem)
+namespace intel_8080 {
+
+CPU8080::CPU8080(std::shared_ptr<intel_8080::Memory8080> new_mem)
     : mem_access_(std::move(new_mem)) {
   // Registers: B=0, C=1, D=2, E=3, H=4, L=5 Memory=6, A=7
   registers_.fill(0x00);
   stack_pointer_ = 0x0000;
   program_counter_ = 0x0000;
-  flags_ = 0x02;  // 00000010
+  flags_ = Flags();  // 00000010
   INTE_ = false;
   mem_access_ = new_mem;
 };
 
-void intel_8080::CPU8080::step() {
+std::uint8_t CPU8080::Flags::psw() {
+  std::uint8_t return_byte = 0x02;
+
+  // Shift flags to proper location
+  return_byte = return_byte | (sign << 7);
+  return_byte = return_byte | (zero << 6);
+  return_byte = return_byte | (aux_carry << 4);
+  return_byte = return_byte | (parity << 2);
+  return_byte = return_byte | carry;
+  return return_byte;
+};
+
+void CPU8080::step() {
   std::uint8_t instruction = fetch_byte();
   execute(instruction);
 };
 
-std::uint8_t intel_8080::CPU8080::fetch_byte() {
+std::uint8_t CPU8080::fetch_byte() {
   std::uint8_t byte = mem_access_->Read(program_counter_);
   program_counter_++;
   return byte;
 };
 
-std::uint8_t intel_8080::CPU8080::fetch_byte(std::uint16_t mem_location) {
+std::uint8_t CPU8080::fetch_byte(std::uint16_t mem_location) {
   std::uint8_t byte = mem_access_->Read(mem_location);
   return byte;
 };
 
-std::uint16_t intel_8080::CPU8080::fetch_word() {
+std::uint16_t CPU8080::fetch_word() {
   std::uint8_t low_byte = fetch_byte();
   std::uint8_t high_byte = fetch_byte();
   std::uint16_t word = ((std::uint16_t)high_byte << 8) | low_byte;
   return word;
 }
 
-std::uint16_t intel_8080::CPU8080::fetch_word(std::uint16_t mem_location) {
+std::uint16_t CPU8080::fetch_word(std::uint16_t mem_location) {
   std::uint8_t low_byte = mem_access_->Read(mem_location);
   std::uint8_t high_byte = mem_access_->Read(mem_location + 1);
   std::uint16_t word = ((std::uint16_t)high_byte << 8) | low_byte;
   return word;
 };
 
-void intel_8080::CPU8080::reset() { program_counter_ = 0x0000; };
+void CPU8080::reset() { program_counter_ = 0x0000; };
 
-void intel_8080::CPU8080::execute(std::uint8_t opcode) {
+void CPU8080::execute(std::uint8_t opcode) {
   /*
    Will utilize an arrray with indicees 0 to 256 for all opcodes found in
    the intel 8080. Will use a struct containg data for the specific instruction,
    source and/or destination, pointer to generic methods such as MOV, ADD, etc.
    */
 }
+}  // namespace intel_8080
--- src/CPU8080.h
+++ src/CPU8080.h
@@ -6,7 +6,29 @@
 #include "Memory8080.h"
 
 namespace intel_8080 {
+
 class CPU8080 {
+  struct Port {
+    std::uint8_t bit0 : 1;
+    std::uint8_t bit1 : 1;
+    std::uint8_t bit2 : 1;
+    std::uint8_t bit3 : 1;
+    std::uint8_t bit4 : 1;
+    std::uint8_t bit5 : 1;
+    std::uint8_t bit6 : 1;
+    std::uint8_t bit7 : 1;
+  };
+
+  struct Flags {
+    std::uint8_t sign : 1;
+    std::uint8_t zero : 1;
+    std::uint8_t aux_carry : 1;
+    std::uint8_t parity : 1;
+    std::uint8_t carry : 1;
+
+    std::uint8_t psw();
+  };
+
  public:
   CPU8080(std::shared_ptr<intel_8080::Memory8080> new_mem);
 
@@ -28,7 +50,7 @@ class CPU8080 {
   std::array<std::uint8_t, 8> registers_;
   std::uint16_t stack_pointer_;
   std::uint16_t program_counter_;
-  std::uint8_t flags_;
+  Flags flags_;
   // false = 0 and true = 1
   bool INTE_;
   std::shared_ptr<intel_8080::Memory8080> mem_access_;
