8dc5e8ed0189b4eb79e0cd30fa82934cfa1c372d
josh-benge <bengej@oregonstate.edu>
2026-05-08 21:01:27 -0400

    Finished Single Register Instructions

--- src/Instructions8080.cc
+++ src/Instructions8080.cc
@@ -1,3 +1,5 @@
+#include <cstdint>
+
 #include "CPU8080.h"
 
 namespace intel_8080 {
@@ -31,16 +33,39 @@ void CPU8080::rlc() {
 
 // STAX: Store Accumulator
 // Contents of A are stoed in the memory location from either BC, or DE.
-// No flags affected
+// Flags affected: N/A
 void CPU8080::stax(uint16_t mem_location) {
   mem_access_->write(mem_location, registers_.reg_a);
 }
 
 // LDAX: Load Accumulator
 // Contents of memory location from either BC, or DE are stored in Accumulator
-// No flags affected
+// Flags affected: N/A
 void CPU8080::ldax(uint16_t mem_location) {
   registers_.reg_a = mem_access_->read(mem_location);
 }
+
+// CMC: Complement carry
+// Flips the value of Carry flag.
+// Flags affected: Carry
+void CPU8080::cmc() { flags_.carry = flags_.carry ^ 0x01; }
+
+// STC: Set carry
+// Sets the carry to 1
+// Flags affected: Carry
+void CPU8080::stc() { flags_.carry = 1; }
+
+// DCR: Decrement Register/Memory
+// Reduces the value of the register or memory by 1.
+// Flags affected: Zero, Sign, Parity, Aux Carry.
+void CPU8080::dcr(uint8_t* byte) {
+  *byte -= 1;
+  update_flags_szp(*byte);
+}
+
+// CMA: Complement Accumulator
+// Each bit of the accumulator is complemented
+// Flags affected: N/A
+void CPU8080::cma() { registers_.reg_a = ~registers_.reg_a; }
 
 }  // namespace intel_8080
