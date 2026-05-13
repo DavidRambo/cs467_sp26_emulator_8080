0a3dc52eba12f224c66aa769c2635d3e8af7828f
David Rambo <davidrambo@mailfence.com>
2026-05-13 12:54:06 -0700

    test: Player 1 movement data

--- tests/test_io.cc
+++ tests/test_io.cc
@@ -15,21 +15,63 @@ TEST_CASE("Test IN input instruction with InputHandler") {
   std::vector<uint8_t> data = {0xdb, 1};  // opcode, port number
   mem->load_data(data);
 
-  // Player 1 start = Port 1 bit 2
-  input_handler->handle_key_press(SDL_SCANCODE_KP_ENTER);
+  SUBCASE("Player 1 Start") {
+    // Player 1 start = Port 1 bit 2
+    input_handler->handle_key_press(SDL_SCANCODE_KP_ENTER);
 
-  emu.step();
-  intel_8080::CPU8080::State state = emu.get_state();
-  CHECK_EQ(state.registers.reg_b, 0);
-  CHECK_EQ(state.registers.reg_c, 0);
-  // Check that bit 2 is set. Port 1 bit 3 is always 1.
-  CHECK_EQ(state.registers.reg_a, 0b1100);
-  CHECK_EQ(state.registers.reg_d, 0);
-  CHECK_EQ(state.registers.reg_h, 0);
-  CHECK_EQ(state.registers.reg_l, 0);
-  CHECK_EQ(state.program_counter, 2);
-  CHECK_EQ(state.flags.sign, 0);
-  CHECK_EQ(state.flags.zero, 0);
-  CHECK_EQ(state.flags.parity, 0);
-  CHECK_EQ(state.flags.carry, 0);
+    emu.step();
+    intel_8080::CPU8080::State state = emu.get_state();
+    CHECK_EQ(state.registers.reg_b, 0);
+    CHECK_EQ(state.registers.reg_c, 0);
+    // Check that bit 2 is set. Port 1 bit 3 is always 1.
+    CHECK_EQ(state.registers.reg_a, 0b1100);
+    CHECK_EQ(state.registers.reg_d, 0);
+    CHECK_EQ(state.registers.reg_h, 0);
+    CHECK_EQ(state.registers.reg_l, 0);
+    CHECK_EQ(state.program_counter, 2);
+    CHECK_EQ(state.flags.sign, 0);
+    CHECK_EQ(state.flags.zero, 0);
+    CHECK_EQ(state.flags.parity, 0);
+    CHECK_EQ(state.flags.carry, 0);
+  }
+
+  SUBCASE("Player 1 Move Left") {
+    // Player 1 start = Port 1 bit 2
+    input_handler->handle_key_press(SDL_SCANCODE_LEFT);
+
+    emu.step();
+    intel_8080::CPU8080::State state = emu.get_state();
+    CHECK_EQ(state.registers.reg_b, 0);
+    CHECK_EQ(state.registers.reg_c, 0);
+    // Check that bit 5 is set. Port 1 bit 3 is always 1.
+    CHECK_EQ(state.registers.reg_a, 0b10'1000);
+    CHECK_EQ(state.registers.reg_d, 0);
+    CHECK_EQ(state.registers.reg_h, 0);
+    CHECK_EQ(state.registers.reg_l, 0);
+    CHECK_EQ(state.program_counter, 2);
+    CHECK_EQ(state.flags.sign, 0);
+    CHECK_EQ(state.flags.zero, 0);
+    CHECK_EQ(state.flags.parity, 0);
+    CHECK_EQ(state.flags.carry, 0);
+  }
+
+  SUBCASE("Player 1 Move Right") {
+    // Player 1 start = Port 1 bit 2
+    input_handler->handle_key_press(SDL_SCANCODE_RIGHT);
+
+    emu.step();
+    intel_8080::CPU8080::State state = emu.get_state();
+    CHECK_EQ(state.registers.reg_b, 0);
+    CHECK_EQ(state.registers.reg_c, 0);
+    // Check that bit 6 is set. Port 1 bit 3 is always 1.
+    CHECK_EQ(state.registers.reg_a, 0b100'1000);
+    CHECK_EQ(state.registers.reg_d, 0);
+    CHECK_EQ(state.registers.reg_h, 0);
+    CHECK_EQ(state.registers.reg_l, 0);
+    CHECK_EQ(state.program_counter, 2);
+    CHECK_EQ(state.flags.sign, 0);
+    CHECK_EQ(state.flags.zero, 0);
+    CHECK_EQ(state.flags.parity, 0);
+    CHECK_EQ(state.flags.carry, 0);
+  }
 }
