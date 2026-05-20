2c593549ad36df0c6ff4d5fe359f28ab01795193
David Rambo <davidrambo@mailfence.com>
2026-05-20 12:22:42 -0700

    integrate GameWindow into main

--- src/main.cc
+++ src/main.cc
@@ -4,8 +4,10 @@
 
 #include "AudioMixer.h"
 #include "CPU8080.h"
+#include "GameWindow.h"
 #include "Input.h"
 #include "Memory8080.h"
+
 int main(int argc, char* argv[]) {
   if (argc > 5) {
     std::cerr << "Too many arguments. Usage: ./emu8008 invaders.h invaders.g "
@@ -21,20 +23,20 @@ int main(int argc, char* argv[]) {
     // return SDL_APP_FAILURE;
   }
 
+  // Initialize and fill memory first, so it can be passed to the GameWindow.
+  std::shared_ptr<intel_8080::Memory8080> mem =
+      std::make_shared<intel_8080::Memory8080>(intel_8080::Memory8080());
+
+  // Load ROM
+  // mem->load_rom(argv[1]); // invaders ROM as one file
+  mem->load_rom_at_addr(argv[1], 0);       // invaders.h
+  mem->load_rom_at_addr(argv[2], 0x800);   // invaders.g
+  mem->load_rom_at_addr(argv[3], 0x1000);  // invaders.f
+  mem->load_rom_at_addr(argv[4], 0x1800);  // invaders.e
+
   // Create GameWindow
-  SDL_Window* window;
-  SDL_Renderer* renderer;
-  if (!SDL_CreateWindowAndRenderer("Test SDL3", kWindowWidth, kWindowHeight,
-                                   SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-    SDL_Log("Failed to create window and renderer.");
-    return SDL_APP_FAILURE;
-  }
-
-  SDL_SetRenderLogicalPresentation(renderer, kWindowWidth, kWindowHeight,
-                                   SDL_LOGICAL_PRESENTATION_LETTERBOX);
-
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderPresent(renderer);
+  graphics_display::GameWindow game_window = graphics_display::GameWindow(
+      mem->get_vram_span(), kWindowWidth, kWindowHeight, "Space Invaders");
 
   std::shared_ptr<audio::Mixer> mixer =
       std::make_shared<audio::Mixer>(audio::Mixer());
@@ -42,16 +44,6 @@ int main(int argc, char* argv[]) {
   // Shared with cpu and main loop for event polling.
   std::shared_ptr<input::InputHandler> input_handler =
       std::make_shared<input::InputHandler>();
-
-  std::shared_ptr<intel_8080::Memory8080> mem =
-      std::make_shared<intel_8080::Memory8080>(intel_8080::Memory8080());
-
-  // TODO: Load ROM
-  // mem->load_rom(argv[1]); // invaders ROM as one file
-  mem->load_rom_at_addr(argv[1], 0);       // invaders.h
-  mem->load_rom_at_addr(argv[2], 0x800);   // invaders.g
-  mem->load_rom_at_addr(argv[3], 0x1000);  // invaders.f
-  mem->load_rom_at_addr(argv[4], 0x1800);  // invaders.e
 
   intel_8080::CPU8080 cpu = intel_8080::CPU8080(mem, input_handler, mixer);
 
@@ -66,7 +58,7 @@ int main(int argc, char* argv[]) {
       continue;
     }
 
-    // Run CPU for 16 ns to approximate 60 fps.
+    // Run CPU for 16 ms to approximate 60 fps.
     Uint64 current_tick = SDL_GetTicks();
     while (current_tick < start_tick + 16) {
       cpu.step();
@@ -77,12 +69,6 @@ int main(int argc, char* argv[]) {
   }
 
   // Shut down application.
-  if (renderer) {
-    SDL_DestroyRenderer(renderer);
-  }
-  if (window) {
-    SDL_DestroyWindow(window);
-  }
   SDL_Quit();
 
   return 0;
