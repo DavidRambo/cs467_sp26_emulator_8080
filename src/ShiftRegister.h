a997f8c1f69e102fcfa30f131f1263a04152dec3
David Rambo <davidrambo@mailfence.com>
2026-05-20 19:52:01 -0700

    doc: improve method documentation

--- src/ShiftRegister.h
+++ src/ShiftRegister.h
@@ -5,15 +5,27 @@
 namespace hardware {
 class ShiftRegister {
  public:
-  // [IN 3] Returned value is fed into IN 3
+  // Returns 8 bits from the buffer_ starting at offset_ bits from the most
+  // significant bit.
+  //
+  // For example: an offset_ of 2 would returns bits 13 through
+  // 5 from the buffer_.
+  //
+  // Triggered by the [IN 3] instruction to place a byte into the data bus,
+  // which is then transfered to the accumulator by the 8080.
   uint8_t GetShiftedByte();
 
-  // [OUT 4] Accumulator bits passed in from OUT 4 opcode
-  // This loads a 16 bit pixel buffer 8 bits at a time
+  // Right-shifts the buffer_ by 8 bits and then loads accumulator_bits into the
+  // buffer_'s 8 most significant bits.
+  //
+  // Triggered by the [OUT 4] instruction, which loads the accumulator's data
+  // to the data bus for the Write 4 device.
   uint16_t LoadBuffer(uint8_t accumulator_bits);
 
-  // [OUT 2] Accumulator bits passed in from OUT 2 opcode
-  // This sets how many shifts are to be taken
+  // Sets the shift register's offset for when data is fetched.
+  //
+  // Triggered by the [OUT 2] instruction, which loads the accumulator's data
+  // to the data bus for the Write 2 device.
   uint8_t SetOffset(uint8_t accumulator_bits);
 
  private:
