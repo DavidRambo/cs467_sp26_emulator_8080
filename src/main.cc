1b0e3af25163dc8169a2792c78feb4a6138edffc
Pat Keleher <122421655+PatKeleher@users.noreply.github.com>
2026-05-21 21:30:30 -0500

    main loop playgroud

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,31 @@
 #include "CPU8080.h"
 #include "Memory8080.h"
+#include "AudioMixer.h"
+#include "GameWindow.h"
+#include "SpaceInvadersVRamDecoder.h"
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL.h>
 
-int main() { return 0; }
+int main() 
+{ 
+    constexpr int kRows = 224;
+    constexpr int kCols = 256;
+
+    SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+
+    std::unique_ptr<intel_8080::Memory8080> memory = 
+        std::make_unique<intel_8080::Memory8080>(intel_8080::Memory8080());
+
+    memory->load_rom("./invaders/invaders_all.txt");
+
+    graphics_display::GameWindow display(nullptr, kCols, kRows, "SpaceInvaders");
+    audio::Mixer sound;
+
+    for (int i = 0; i < 5; i++) {
+        SDL_Delay(2000);
+    }
+    SDL_Delay(5000);
+
+    SDL_Quit();
+    return 0; 
+}
