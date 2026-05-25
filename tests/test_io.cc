4768194f1fca10830a49cf07b4f9d8cf86272e6b
David Rambo <davidrambo@mailfence.com>
2026-05-24 18:04:56 -0700

    fix(test): RETURN instead of KP_ENTER

--- tests/test_io.cc
+++ tests/test_io.cc
@@ -22,7 +22,7 @@ TEST_CASE("Test IN input instruction with InputHandler") {
 
   SUBCASE("Player 1 Start") {
     // Player 1 start = Port 1 bit 2
-    input_handler->HandleKeyPress(SDL_SCANCODE_KP_ENTER);
+    input_handler->HandleKeyPress(SDL_SCANCODE_RETURN);
 
     emu.step();
     intel_8080::CPU8080::State state = emu.get_state();
